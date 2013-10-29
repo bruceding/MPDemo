b8841825111585fd2739dcb0d8faad0e1663e38e
bruce ding <dingjingdjdj@gmail.com>
2013-10-29 08:08:50 +0800

    增加新类型

--- src/MsgController.php
+++ src/MsgController.php
@@ -1,17 +1,50 @@
 <?php
 require 'TextMsg.php';
 require 'EventMsg.php';
+require 'ImageMsg.php';
+require 'LinkMsg.php';
+require 'LocationMsg.php';
 
 class MsgController{
     
+    /**
+     * 消息类型：文本 
+     */
     const MESSAGE_TYPE_TEXT = "text";
 
+    /**
+     * 消息类型：图片 
+     */
+    const MESSAGE_TYPE_IMAGE = "image";
+
+    /**
+     * 消息类型：链接消息 
+     */
+    const MESSAGE_TYPE_LINK = "link";
+
+    /**
+     * 消息类型：位置消息 
+     */
+    const MESSAGE_TYPE_LOCATION = "location";
+
+    /**
+     * 消息类型：事件 
+     */
+    const MESSAGE_TYPE_EVENT = "event";
+
     public static function factory($type = '') {
-        
-        if (strtolower($type) == 'text') {
+
+        $type = strtolower($type);
+        if ($type == self::MESSAGE_TYPE_TEXT) {
             return new TextMsg();
-        } else if (strtolower($type) == 'event') {
+        } else if ($type == self::MESSAGE_TYPE_EVENT) {
         	return new EventMsg();
+        } else if ($type == self::MESSAGE_TYPE_IMAGE) {
+            return new ImageMsg();
+        } else if ($type == self::MESSAGE_TYPE_LINK) {
+            return new LinkMsg();
+        } else if ($type == self::MESSAGE_TYPE_LOCATION) {
+            return new LocationMsg();
         }
     }
 }
