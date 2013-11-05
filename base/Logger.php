22e6d3235f66285c7efe7ae937849b8a62a2824a
bruce ding <dingjingdjdj@gmail.com>
2013-11-05 09:03:10 +0800

    增加Logger类，记录日志

---
+++ base/Logger.php
@@ -0,0 +1,159 @@
+<?php
+/**
+ * Logger 
+ * 日志类 
+ *
+ * @package 
+ * @version $id$
+ * @copyright 1997-2005 The PHP Group
+ * @author Tobias Schlitt <toby@php.net> 
+ * @license PHP Version 3.0 {@link http://www.php.net/license/3_0.txt}
+ */
+class Logger {
+
+    //private static $_str = "";
+
+    /**
+     * 日志级别:错误 
+     */
+    const LOG_LEVEL_ERROR = 1;
+
+    /**
+     * 日志级别：警告 
+     */
+    const LOG_LEVEL_WARNING = 2;
+
+    /**
+     * 日志级别：调试 
+     */
+    const LOG_LEVEL_DEBUG = 3;
+
+    /**
+     * Log文件的根目录 
+     */
+    private static $_log_dir;
+
+    /**
+     * 过滤掉不需要记录的路径, 用于_getLogDirectory函数  
+     */
+    //private static $_unuse_dir = array('include');
+
+    public static function error($e) {
+        
+        $file = self::_getLogDirectory($e);
+        return self::log($e, self::LOG_LEVEL_ERROR, $file);
+    }
+
+    private static function _getLogDirectory($e) {
+       
+        if ($e instanceof Exception) {
+            $path = $e->getFile();
+            $path = explode('/', $path);
+
+            $rootPath = explode('/', ROOT_PATH);
+            /**
+            foreach ($path as $key => $value) {
+
+                // 过滤掉不需要的路径
+                if (in_array($value, self::$_unuse_dir)) {
+                    unset($path[$key]);
+                    break;
+                } 
+                unset($path[$key]);
+            }
+            **/
+
+            $file = '';
+            foreach ($path as $key => $value) {
+
+                // 过滤掉根目录
+                if (in_array($value, $rootPath)) {
+                    continue;
+                }
+
+                $file .= $value . '/';
+            }
+            $file = substr($file, 0, strpos($file, '.'));
+
+            return $file . '/' . date('Y-m-d') . '.log';
+        }
+
+    }
+
+    public static function warning($e, $file = '') {
+        self::log($e, self::LOG_LEVEL_WARNING, $file);
+    }
+    
+    public static function debug($e, $file='') {
+        self::log($e, self::LOG_LEVEL_DEBUG, $file); 
+    }
+    public static function log($e, $level, $file) {
+
+        self::validLogDir();
+
+        if ($file) {
+            $fileArr = explode('/' , $file);
+            $dir = self::$_log_dir;
+            for ($i = 0; $i < count($fileArr) -1; $i++) {
+               $dir .= '/' . $fileArr[$i]; 
+            }
+            if (!file_exists($dir)) {
+                mkdir($dir, 0777, true);
+            }
+            $file = self::$_log_dir . '/' . $file;
+        } else {
+            $file = self::$_log_dir . '/' . date('Y-m-d') . '.log';
+        }
+
+        $message = $logstr = "";
+        if ($e instanceof Exception) {
+            $message .=  $e->getFile() . " : " . $e->getMessage();
+        } else {
+            $message = $e;
+        }
+
+        switch($level) {
+            case self::LOG_LEVEL_ERROR:
+                $logstr .= "[" . date('Y-m-d H:i:s') . "]" . " Error: " . $message . "\n";
+                break;
+            case self::LOG_LEVEL_WARNING:
+                $logstr .= "[" . date('Y-m-d H:i:s') . "]" . " Warning: " . $message . "\n";
+                break;
+            case self::LOG_LEVEL_DEBUG:
+                $logstr .= "[" . date('Y-m-d H:i:s') . "]" . " Debug: " . $message . "\n";
+                break;
+        }
+
+        file_put_contents($file, $logstr, FILE_APPEND);
+    }
+
+    /**
+     * validLogDir 
+     * 根据LOG_DIR变量，定义Log根目录 
+     *
+     * @static
+     * @access public
+     * @return void
+     */
+    public static function validLogDir() {
+
+        if (!defined('LOG_DIR') && !$conf['LOG_DIR']) {
+            die('NOT FOUND LOG_DIR  VARIABLE');
+        }
+
+        if (defined('LOG_DIR')) { 
+        
+            $logDir = ROOT_PATH . '/' . LOG_DIR; 
+        } else if ($conf['LOG_DIR']) {
+        
+            $logDir = ROOT_PATH . '/' . $conf['LOG_DIR']; 
+        }
+
+        if (!file_exists($logDir)) {
+            
+            mkdir($logDir, 0777, true);
+        }
+        
+        self::$_log_dir = $logDir;
+    }
+}
--- index.php
+++ index.php
@@ -5,18 +5,14 @@ define('ROOT_PATH', __DIR__);
 
 require ROOT_PATH . '/src/config.php';
 require ROOT_PATH . '/src/MpApi.php';
-
-/**
-  * wechat php test
-  */
+require ROOT_PATH . '/base/Logger.php';
 
 $api = new MpApi();
 try {
     $api->dealMsg();
 } catch (Exception $e) {
 
-    // 简单的进行错误处理
-    file_put_contents(ROOT_PATH . '/error.log', $e->getMessage(), FILE_APPEND);
+    Logger::error($e);
 }
 exit;
 ?>
